f8f4b0002d6bfe03c0ef984e80aa54768250d352
jjdeharo <jjdeharo@gmail.com>
2015-04-03 22:34:38 +0200

    Kruskal-Wallis

---
+++ estadistica/kruskal Wallis.R
@@ -0,0 +1,21 @@
+# Comparación de medias, test de Kruskal-Wallis
+# Prueba no paramétrica
+
+x1 <- rnorm(100,10)
+x2 <- rnorm(100,10.5)
+x3 <- rnorm(100,10)
+
+# Primera opción: mediante factores
+x <- c(x1,x2,x3) # Unión de las 3 variables en una
+f <- factor(c(rep("x1",100),rep("x2",100),rep("x3",100))) # Factores
+
+test1 <- kruskal.test(x~f)
+
+print(test1)
+
+# Segunda opción: data.frame
+x<- data.frame(x1=x1,x2=x2,x3=x3)
+
+test2 <- kruskal.test(x)
+
+print(test2)
