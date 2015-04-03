add68e63bf01e379592a7a55da4b85dd270aa120
jjdeharo <jjdeharo@gmail.com>
2015-04-03 22:40:03 +0200

    ANOVA

---
+++ estadistica/ANOVA_un_nivel.R
@@ -0,0 +1,15 @@
+# Comparación de medias, ANOVA
+# Prueba paramétrica
+
+x1 <- rnorm(100,10)
+x2 <- rnorm(100,10.5)
+x3 <- rnorm(100,10)
+
+# Primera opción: mediante factores
+x <- c(x1,x2,x3) # Unión de las 3 variables en una
+f <- factor(c(rep("x1",100),rep("x2",100),rep("x3",100))) # Factores
+
+test <- aov(x~f)
+
+print(summary(test))
+
