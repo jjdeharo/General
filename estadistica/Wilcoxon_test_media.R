7ca2fefde737fb7d897a3eb08d8470aeb1145d6d
jjdeharo <jjdeharo@gmail.com>
2015-04-03 22:23:52 +0200

    Wilcoxon test media

---
+++ estadistica/Wilcoxon_test_media.R
@@ -0,0 +1,9 @@
+# Comparación una media con un valor teórico, suma de rangos de Wilcoxon
+# Prueba no paramétrica
+
+x <- rnorm(100,10)
+Media <- 10
+
+test <- wilcox.test(x, mu=Media)
+
+print(test)
