ec985289d0b30649f2465e911ef9a9390460f186
jjdeharo <jjdeharo@gmail.com>
2015-04-03 23:50:42 +0200

    Test de Levene

---
+++ estadistica/Levene_test.R
@@ -0,0 +1,15 @@
+# Comparación de varianzas, más de dos muestras
+# Test de Levene
+
+require(car)
+
+set.seed(10)
+x1 <- rnorm(30,10,1)
+x2 <- rnorm(30,10,1.1)
+x3 <- rnorm(30,10,1.2)
+ 
+f <- factor(c(rep("x1",30),rep("x2",30),rep("x3",30)))
+x <- c(x1,x2,x3)
+
+var <- leveneTest(x ~ f)
+print(var)
