9344dea25ccc301647d34d02b118bf0df8ac388c
jjdeharo <jjdeharo@gmail.com>
2015-04-03 23:53:23 +0200

    Test de Bartlett

---
+++ estadistica/Bartlett_test.R
@@ -0,0 +1,12 @@
+# Comparación de varianzas, más de dos muestras
+# Test de Bartlett
+
+set.seed(10)
+x1 <- rnorm(30,10,1)
+x2 <- rnorm(30,10,1.1)
+x3 <- rnorm(30,10,1.2)
+ 
+x <- data.frame(x1=x1,x2=x2,x3=x3)
+
+var <- bartlett.test(x)
+print(var)
