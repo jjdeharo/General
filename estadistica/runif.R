bc82fe4b9feb200e0451b8f786cada6bad12901c
jjdeharo <jjdeharo@gmail.com>
2015-04-04 21:08:25 +0200

    runif

---
+++ estadistica/runif.R
@@ -0,0 +1,10 @@
+# Datos aleatorios equiprobables, distribución uniforme
+
+# Ejemplo 1
+# 10 números al azar entre 1 y 100
+print(runif(10,1,100))
+
+# Ejemplo 2
+# 1000 números enteros al azar entre 1 y 10 y hacemos un histograma con ellos
+nums <- trunc(runif(1000,1,101))
+hist(nums)
