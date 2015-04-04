5d2ee3ad5e2b0d0f6c308626e9b3e471ccb6ef60
jjdeharo <jjdeharo@gmail.com>
2015-04-04 17:45:14 +0200

    Moda

--- estadistica/moda.R
+++ estadistica/moda.R
@@ -1,14 +1,8 @@
 # Calcula la moda de un conjunto de frecuencias
-# Devuelve NULL si no hay moda
+
 
 moda <- function(x) {
-  tabla <- table(x)
-  t <- as.numeric(tabla)
-  
-  frec <- -sort(-tabla)[1]
-  n <- sum(frec == tabla)
-  if (n == length(t)) return(NULL)
-  moda <- as.numeric(names(sort(-tabla))[1:n])
-  return(moda)
+  t <- table(x)
+  return(as.numeric(names(t)[t == max(t)]))
   
 }
