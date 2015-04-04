6a361a3ad2c9043eaac5ab3b2339887a2d54ecea
jjdeharo <jjdeharo@gmail.com>
2015-04-04 14:38:03 +0200

    Estadisticos

--- estadistica/Estadisticos.R
+++ estadistica/Estadisticos.R
@@ -2,4 +2,9 @@
 
 x <- rnorm(100,10,2)
 
-cat("Media=",mean(x),"\nMediana=",median(x),"\nVarianza=",var(x),"\nDesviación típica=",sd(x))
+cat("Media=",mean(x),"\nMediana=",median(x),"\nVarianza=",var(x),"\nDesviación típica=",sd(x),"\n\n")
+
+print(quantile(x))
+
+p <- c(0,1/3,2/3,1)
+print(quantile(x,p=p))
