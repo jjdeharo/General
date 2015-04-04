c2d5779363c364ec19214ea6ef33463fd75ca65f
jjdeharo <jjdeharo@gmail.com>
2015-04-04 21:12:30 +0200

    rnorm

---
+++ estadistica/rnorm.R
@@ -0,0 +1,10 @@
+# Datos aleatorios normales, distribución normal
+
+# Ejemplo 1
+# 10 números al azar,media 100 y desviación típica 2
+print(rnorm(10,100,2))
+
+# Ejemplo 2
+# 1000 números enteros al azar entre 1 y 10 con la misma media y desviación de antes y hacemos un histograma con ellos
+nums <- trunc(rnorm(1000,100,2))
+hist(nums)
